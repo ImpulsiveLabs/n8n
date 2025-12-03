6d019d78ff7e545fe30653c1c6e5073e2ffdf7a7
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-12-03 12:33:18 +0200

    feat(core): Block access to env in code and expressions by default (#22643)

--- packages/cli/src/deprecation/__tests__/deprecation.service.test.ts
+++ packages/cli/src/deprecation/__tests__/deprecation.service.test.ts
@@ -20,7 +20,6 @@ describe('DeprecationService', () => {
 		// Ignore environment variables coming in from the environment when running
 		// this test suite.
 		process.env = {
-			N8N_BLOCK_ENV_ACCESS_IN_NODE: 'false',
 			N8N_GIT_NODE_DISABLE_BARE_REPOS: 'false',
 		};
 
@@ -90,7 +89,6 @@ describe('DeprecationService', () => {
 		beforeEach(() => {
 			process.env = {
 				N8N_RUNNERS_ENABLED: 'true',
-				N8N_BLOCK_ENV_ACCESS_IN_NODE: 'false',
 				N8N_GIT_NODE_DISABLE_BARE_REPOS: 'false',
 			};
 		});
@@ -183,37 +181,10 @@ describe('DeprecationService', () => {
 		});
 	});
 
-	describe('N8N_BLOCK_ENV_ACCESS_IN_NODE', () => {
-		beforeEach(() => {
-			process.env = {
-				N8N_RUNNERS_ENABLED: 'true',
-				N8N_GIT_NODE_DISABLE_BARE_REPOS: 'false',
-			};
-
-			jest.resetAllMocks();
-		});
-
-		test('should warn when N8N_BLOCK_ENV_ACCESS_IN_NODE is not set', () => {
-			delete process.env.N8N_BLOCK_ENV_ACCESS_IN_NODE;
-			deprecationService.warn();
-			expect(logger.warn).toHaveBeenCalled();
-		});
-
-		test.each(['false', 'true'])(
-			'should not warn when N8N_BLOCK_ENV_ACCESS_IN_NODE is %s',
-			(value) => {
-				process.env.N8N_BLOCK_ENV_ACCESS_IN_NODE = value;
-				deprecationService.warn();
-				expect(logger.warn).not.toHaveBeenCalled();
-			},
-		);
-	});
-
 	describe('N8N_GIT_NODE_DISABLE_BARE_REPOS', () => {
 		beforeEach(() => {
 			process.env = {
 				N8N_RUNNERS_ENABLED: 'true',
-				N8N_BLOCK_ENV_ACCESS_IN_NODE: 'false',
 			};
 			jest.resetAllMocks();
 		});
